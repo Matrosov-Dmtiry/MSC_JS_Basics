a51b1a7958938d18cc7075bb1e4f55375da8fdbd
Дмитрий Матросов <Matrsov.DE@gmail.com>
2019-12-13 02:44:11 +0300

    practice task

--- js/index.js
+++ js/index.js
@@ -1,12 +1,9 @@
-let reserve = Number(prompt('Сколько у вас с собой есть денег?'));
-let appleCounter = Number(prompt('Сколько вы купили яблок?'));
-let breadCounter =  Number(prompt('Сколько вы купили батонов хлеба?'));
+let number = Number(prompt('Введите число'));
 
-let oneApplePrice = Number(prompt('Сколько стоит одно яблоко?'));
-let oneBreadPrice = Number(prompt('Сколько стоит один батон хлеба?'));
-
-let result = (reserve - appleCounter * oneApplePrice - breadCounter * oneBreadPrice) > 0
-
-let template = '<h1>' + result + '</h1>';
-
-document.body.innerHTML = template;
+if (number <= 0) {
+	console.log(number);
+} else if (number > 0) {
+	console.log(Number('-' + number));
+} else {
+	console.log('не число');
+}
