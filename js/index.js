fabd75ef9de60ad65f8bf50c5085e5f31ca951a8
Дмитрий Матросов <Matrsov.DE@gmail.com>
2019-12-13 02:00:28 +0300

    practice task 3

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
-let reserve = Number(prompt('Сколько у вас с собой есть денег?'));
-let appleCounter = Number(prompt('Сколько вы купили яблок?'));
-let breadCounter =  Number(prompt('Сколько вы купили батонов хлеба?'));
+const bodyElement = document.body;
+const pageElement = document.querySelectorAll('.page')[0];
+const personElement = document.querySelectorAll('span.name')[0];
+const imgElement = document.querySelectorAll('img')[0];
+const bioElement = document.querySelectorAll('.shortBio')[0];
 
-let oneApplePrice = Number(prompt('Сколько стоит одно яблоко?'));
-let oneBreadPrice = Number(prompt('Сколько стоит один батон хлеба?'));
+let background = prompt('Какой фон будет у страницы?');
+let textColor = prompt('Какой будет цвет текста на странице?');
+let person = prompt('Как зовут человека, который вас вдохновляет');
+let imgSrc = prompt('Введите адрес картинки');
+let bioText = prompt('Введите текст страницы');
 
-let result = (reserve - appleCounter * oneApplePrice - breadCounter * oneBreadPrice) > 0
-
-let template = '<h1>' + result + '</h1>';
-
-document.body.innerHTML = template;
+bodyElement.style.backgroundColor = background;
+pageElement.style.color = textColor;
+personElement.innerHTML = person;
+imgElement.setAttribute('src', imgSrc);
+bioElement.innerHTML = bioText;
+bioElement.className += ' animated' ;
