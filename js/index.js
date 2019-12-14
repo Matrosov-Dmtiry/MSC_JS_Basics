77c944c75b7311e017411fc881446038df9655f8
Дмитрий Матросов <Matrsov.DE@gmail.com>
2019-12-14 03:56:26 +0300

    practice task 4.8.1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
-let number = Number(prompt('Введите число'));
-
-if (number <= 0) {
-	console.log(number);
-} else if (number > 0) {
-	console.log(Number('-' + number));
-} else {
-	console.log('не число');
+function haveEnough(reserve, appleCounter, oneApplePrice, breadCounter, oneBreadPrice) {
+	let result = reserve - appleCounter * oneApplePrice - breadCounter * oneBreadPrice > 0;
+	return result ? 'Вам хватает денег на покупки' : 'Вам не хватает денег';
 }
+
+let template = '<h1>' + haveEnough(
+	Number(prompt('Сколько у вас с собой есть денег?')),
+	Number(prompt('Сколько вы купили яблок?')),
+	Number(prompt('Сколько вы купили батонов хлеба?')),
+	Number(prompt('Сколько стоит одно яблоко?')),
+	Number(prompt('Сколько стоит один батон хлеба?'))
+	) + '</h1>';
+
+document.body.innerHTML = template;
