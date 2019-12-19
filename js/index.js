1c8eee102a8ac61b431a400eed7778d189a99aca
Дмитрий Матросов <Matrsov.DE@gmail.com>
2019-12-19 03:20:37 +0300

    practice task 4

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
-let number = Number(prompt('Введите число'));
+const popUp = document.querySelector('.popup');
+const menu = document.querySelector('.menu');
 
-if (number < 0) {
-	console.log(number/-1);
-} else if (number > 0) {
-	console.log(number);
+const showPopUpBtn = document.querySelector('.request');
+const closePopUpBtn = document.querySelector('.closePopup');
+const showMenuBtn = document.querySelector('.openMenu ');
+const closeMenuBtn = document.querySelector('.closeMenu ');
+
+let popUpState = false;
+let menuState = -50;
+
+const tooglePopUp = function () {
+	if(this === showPopUpBtn) {
+		popUpState = true;
+	} else {
+		popUpState = !popUpState;
+	}
+  popUp.style.display = popUpState ? 'flex' : 'none';
 }
+
+const toogleMenu = function() {
+	menuState = menuState < 0 ? 0 : -50;
+  menu.style.left = menuState + 'vw';
+}
+
+showPopUpBtn.onclick = tooglePopUp;
+closePopUpBtn.onclick = tooglePopUp;
+showMenuBtn.onclick = toogleMenu;
+closeMenuBtn.onclick = toogleMenu;
